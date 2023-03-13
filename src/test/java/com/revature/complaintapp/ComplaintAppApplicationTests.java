52c87ca9cd2a7f27232a9beed502c93912c533dc
brian.spilman <brian.spilman@wsu.edu>
2023-03-13 12:01:14 -0700

    Initial commit

---
+++ src/test/java/com/revature/complaintapp/ComplaintAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.complaintapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
